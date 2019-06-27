4351982289ebdf2fc34ede09dea95b06b01862ff
DRW666 <sd_madandan@163.com>
2019-06-27 13:46:22 +0800

    修改冲突

--- src/components/vip/vipContentshop/index.vue
+++ src/components/vip/vipContentshop/index.vue
@@ -9,7 +9,6 @@
                        <span>{{item.money}}</span>无门槛
                    </div>
                    <div class="shops">
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
                        <a href=""><img :src="item.imgs" alt=""></a>
                        <span>{{item.names}}</span>
                    </div>
@@ -19,16 +18,6 @@
         <div class="vip-footer">
             <span>更多外卖可到美团外卖首页查看</span>
         </div>
-=======
-                       <img :src="item.imgs" alt="">
-                       <span>{{item.names}}</span>
-                   </div>
-                </li>
-            </ul>
-             
-        </div>
-
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
     
     </div>
     
@@ -57,7 +46,6 @@ export default {
                     imgs:"http://p0.meituan.net/waimaipoi/9c7d800065e94a2a6863b31729365a4810966.jpg",
                     names:'御品轩(枫叶新都市店)',
                     path:''
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
                 },
                 {
                     money:'￥7',
@@ -76,8 +64,6 @@ export default {
                     imgs:"http://p0.meituan.net/waimaipoi/9c7d800065e94a2a6863b31729365a4810966.jpg",
                     names:'御品轩(枫叶新都市店)',
                     path:''
-=======
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
                 }
             ]
         }
@@ -87,20 +73,12 @@ export default {
 
 <style scoped>
 .vip-content-text{
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
-    background: #f3f4f4;
-=======
     background: #f2f2f2;
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
 
 }
 ul>li{
     display: block;
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
-     width: 30%;
-=======
      width: 33%;
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
     height: 2.7rem;
   
     background: url('../../../assets/img/vipImg/bgc_wps.png') no-repeat;
@@ -110,45 +88,26 @@ ul>li{
     font-size: 0.3rem;
     color: #777777;
     text-align: center;
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
-    margin-bottom: 0.2rem;
-=======
     margin-bottom: 0.1rem;
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
 }
 .price span{
     font-size: 0.4rem;
     color: #e85b4d;
 }
 .shops>img{
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
-    width: 1.2rem;
-    height: 1rem;
-=======
     width: 1.5rem;
     height: 1.2rem;
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
     margin-top: 0.1rem;
 }
 .oul{
     display: flex;
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
-    flex-wrap: wrap;
-=======
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
 }
 .oul>li{
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
-    margin: 0.1rem 0.1rem 0.2rem 0.15rem; 
-    
-    /* width: 100%; */
-=======
     margin-left: 0.2rem;
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
 }
 .shops{
     display: flex;
@@ -163,7 +122,6 @@ ul>li{
     font-size: 0.3rem;
     color: #33312e;
 }
-<<<<<<< HEAD:src/components/vip/vipContentshop/index.vue
 .vip-footer{
     height: 1rem;
     text-align: center;
@@ -171,6 +129,4 @@ ul>li{
     color: #777777;
 }
 
-=======
->>>>>>> lihao:src/components/vip/vipContent-2/index.vue
 </style>
